6ae6d6946eb50648d8163aeadbf49af70c75ec67
Florin-Ciprian Bodin <orynider@rdslink.ro>
2024-03-19 07:05:05 +0200

    Update ConfigData.php

--- classes/ConfigData.php
+++ classes/ConfigData.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @package AutoIndex
  *
@@ -142,7 +141,43 @@ class ConfigData implements Iterator
 			{
 				continue;
 			}
-			$parts = explode("\t", $line, 2);
+			<?php
+
+/**
+ * @package AutoIndex
+ *
+ * @copyright Copyright (C) 2002-2005 Justin Hagstrom
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ *
+ * @link http://autoindex.sourceforge.net
+ */
+
+/*
+   AutoIndex PHP Script is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   AutoIndex PHP Script is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+if (!defined('IN_AUTOINDEX') || !IN_AUTOINDEX)
+{
+	die();
+}
+
+/**
+ * Reads information stored in files, where the key and data are separated by a
+ * tab.
+ *
+$parts = explode("\t", $line, 2);
 			if (count($parts) !== 2 || $parts[0] == '' || $parts[1] == '')
 			{
 				throw new ExceptionFatal('Incorrect format for file <em>' . Url::html_output($file) . '</em> on line ' . ($i + 1) . '.<br />Format is "variable name[tab]value"');
